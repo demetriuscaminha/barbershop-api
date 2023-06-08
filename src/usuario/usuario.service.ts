2fcfa3ae2e61e0a2ddbe7526bc3522414ca76e46
Demetrius Estacio <201201075157@alunos.estacio.br>
2023-06-08 17:34:55 -0300

    ...

---
+++ src/usuario/usuario.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Usuario } from './usuario.entity';
+import { UsuarioCadastrarDto } from './dto/usuario.cadastrar.dto';
+import { ResultadoDto } from '../../src/dto/resultado.dto';
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+    @Inject('USUARIO_REPOSITORY')
+    private usuarioRepository: Repository<Usuario>,
+  ) {}
+
+  async findAll(): Promise<Usuario[]> {
+    return this.usuarioRepository.find();
+  }
+
+  async cadastrar(data: UsuarioCadastrarDto): Promise<ResultadoDto>{
+    let usuario = new Usuario()
+    usuario.email = data.email
+    usuario.nome = data.nome
+    usuario.password = data.senha
+    usuario.telefone = data.telefone
+    usuario.cpf = data.cpf
+    return this.usuarioRepository.save(usuario)
+    .then((result) => {
+      return <ResultadoDto>{
+        status: true,
+        mensagem: "Usuário cadastrado com sucesso"
+      }
+    })
+    .catch((error) => {
+      return <ResultadoDto>{
+        status: false,
+        mensagem: "Houve um errro ao cadastrar o usuário"
+      }
+    })    
+  }
+}
